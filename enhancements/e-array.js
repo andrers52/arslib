6a447e32171e384f18717c71243da4b788f63ed9
andrers52 <rs.andre@gmail.com>
2024-04-30 00:59:15 -0300

    added range

--- enhancements/e-array.js
+++ enhancements/e-array.js
@@ -1,118 +1,124 @@
-'use strict'
+"use strict";
 
-import Assert from '../util/assert.js'
-import Random from '../util/random.js'
+import Assert from "../util/assert.js";
+import Random from "../util/random.js";
 
-var EArray = {}
+var EArray = {};
 
 // ------------------------------------------------------ //
 // Define the static methods in EArray
 // ------------------------------------------------------ //
 
-EArray.sum = array => array.reduce((total, num) => total + num, 0)
-EArray.mean = array => array.reduce((total, num) => total + num, 0) / array.length
+EArray.sum = (array) => array.reduce((total, num) => total + num, 0);
+EArray.mean = (array) =>
+  array.reduce((total, num) => total + num, 0) / array.length;
 // mean difference, taken two by two (-1 if not applicable)
-EArray.meanDifferenceTwoByTwo = array => {
+EArray.meanDifferenceTwoByTwo = (array) => {
   if (array.length === 0 || array.length === 1)
-    throw ('cannot calculate meanDifferenceTwoByTwo')
-  let diffSum = 0
+    throw "cannot calculate meanDifferenceTwoByTwo";
+  let diffSum = 0;
   for (let i = 1; i < array.length; i++) {
-    diffSum += array[i] - array[i - 1]
+    diffSum += array[i] - array[i - 1];
   }
-  return diffSum / (array.length - 1)
-}
+  return diffSum / (array.length - 1);
+};
 
+EArray.lastIndex = (array) => {
+  return array.length - 1;
+};
 
-EArray.lastIndex = array => {
-  return array.length - 1
-}
+EArray.last = (array) => {
+  return array.length >= 1 ? array[EArray.lastIndex(array)] : "undefined";
+};
 
-EArray.last = array => {
-  return (array.length >= 1) ? array[EArray.lastIndex(array)] : 'undefined'
-}
+EArray.first = (array) => (array.length >= 1 ? array[0] : "undefined");
 
-EArray.first = array => (array.length >= 1) ? array[0] : 'undefined'
+EArray.isLast = (array, item) => item === this.last();
 
-EArray.isLast = (array, item) => item === this.last()
-
-EArray.isFirst = (array, item) => item === this.first()
+EArray.isFirst = (array, item) => item === this.first();
 
 // from array of arrays to single array
 // Note: preserveOriginalArray === true is slower
 EArray.flatten = (array, preserveOriginalArray = true) => {
-  let arrayToUse = preserveOriginalArray ?
-    [...array] : array
-  return arrayToUse.concat.apply([], arrayToUse)
-}
+  let arrayToUse = preserveOriginalArray ? [...array] : array;
+  return arrayToUse.concat.apply([], arrayToUse);
+};
 
 // from 'flat' array to array of arrays of length 'size'
 // Note: preserveOriginalArray === true is slower
 EArray.unflatten = (flattenedArray, size, preserveOriginalArray = true) => {
-  let arrayToUse = preserveOriginalArray ?
-    JSON.parse(JSON.stringify(flattenedArray)) : flattenedArray
-  let resultArray = []
-  while (arrayToUse.length > 0) resultArray.push(arrayToUse.splice(0, size))
-  return resultArray
-}
+  let arrayToUse = preserveOriginalArray
+    ? JSON.parse(JSON.stringify(flattenedArray))
+    : flattenedArray;
+  let resultArray = [];
+  while (arrayToUse.length > 0) resultArray.push(arrayToUse.splice(0, size));
+  return resultArray;
+};
 
-
-EArray.removeLast = array => {
-  array.splice(-1, 1)
-  return array
-}
+EArray.removeLast = (array) => {
+  array.splice(-1, 1);
+  return array;
+};
 
 EArray.clone = (array, cloneFunction) => {
   if (!cloneFunction)
-    return array.map(e =>
-      (Array.isArray(e) || e.clone) ?
-        e.clone() :
-        typeof e === 'object' ?
-          Object.assign({}, e) :
-          e
-    )
+    return array.map((e) =>
+      Array.isArray(e) || e.clone
+        ? e.clone()
+        : typeof e === "object"
+        ? Object.assign({}, e)
+        : e,
+    );
   return array.map(function (item) {
-    return cloneFunction(item)
-  })
-}
+    return cloneFunction(item);
+  });
+};
 
 //get random element from array (with different chances for each one)
 EArray.choiceWithProbabilities = (array, probabilities) => {
-  Assert.assertIsArray(probabilities)
-  Assert.assert(probabilities.length === array.length, 'Probabilities size must be equal to array size')
+  Assert.assertIsArray(probabilities);
+  Assert.assert(
+    probabilities.length === array.length,
+    "Probabilities size must be equal to array size",
+  );
   //each one select a number based on its probability and the
   //one with the bigger number wins.
-  let generatedValues = probabilities.map(prob => Math.random() * prob)
-  let selectedIndex = EArray.indexOfGreaterValue(generatedValues)
-  return array[selectedIndex]
-}
+  let generatedValues = probabilities.map((prob) => Math.random() * prob);
+  let selectedIndex = EArray.indexOfGreaterValue(generatedValues);
+  return array[selectedIndex];
+};
 
 //get random element from array
-EArray.choice = array => array[Random.randomInt(array.length)]
-
+EArray.choice = (array) => array[Random.randomInt(array.length)];
 
 //get random index from array
-EArray.indexChoice = array => Random.randomInt(array.length)
+EArray.indexChoice = (array) => Random.randomInt(array.length);
 
-EArray.indexOfGreaterValue = array => {
-  let greater = array[0]
-  let resultIndex = 0
+EArray.indexOfGreaterValue = (array) => {
+  let greater = array[0];
+  let resultIndex = 0;
   for (let i = 1; i < array.length; i++)
     if (array[i] > greater) {
-      greater = array[i]
-      resultIndex = i
+      greater = array[i];
+      resultIndex = i;
     }
 
-  return resultIndex
-}
+  return resultIndex;
+};
 
-// EArray comprehension
-// range() is a generator that returns all the values between begin and
-// end. We can use it like this:
-// let ten_squares = [i * i for each (i in range(0, 10))];
-// let evens = [i for each (i in range(0, 21)) if (i % 2 === 0)];
-/*
- * function *range(begin, end) { for (let i = begin; i < end; ++i) { yield i; } }
- */
-export { EArray as default }
-export { EArray }
+// EArray.range() is a generator that returns all the values between begin and end-1.
+// We can use it like this:
+// Create a generator object
+// let numbers = EArray.range(1, 5);
+// Use the generator
+// for(let num of numbers) {
+//     console.log(num); // Will print numbers 1 through 4
+// }
 
+EArray.range = function* (begin, end) {
+  for (let i = begin; i < end; ++i) {
+    yield i;
+  }
+};
+
+export { EArray, EArray as default };
