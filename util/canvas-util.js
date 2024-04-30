e6388fef9ca1cfd80f9441709ed50ed12ca91657
andrers52 <rs.andre@gmail.com>
2024-04-30 03:04:31 -0300

    adding jsdoc

--- util/canvas-util.js
+++ util/canvas-util.js
@@ -1,48 +1,68 @@
-'use strict'
+"use strict";
 
-import Util from './util.js'
+import Util from "./util.js";
 
-var CanvasUtil = {}
+var CanvasUtil = {};
 
-// transform Y to go from bottom to top
-CanvasUtil.invertY = (canvas, value) => canvas.height - value
+/**
+ * Transform Y to go from bottom to top
+ *
+ * @param {HTMLCanvasElement} canvas
+ * @param {number} value
+ * @returns {number}
+ */
+CanvasUtil.invertY = (canvas, value) => canvas.height - value;
 
-
-// adjust value and draw on canvas
-CanvasUtil.drawValueOnCanvasLastColumn = (
-  {canvas, value, minValue, maxValue, color}
-) => {
-  let context = canvas.getContext('2d')
+/**
+ * adjust value and draw on canvas
+ * @param {Object} param0
+ * @param {HTMLCanvasElement} param0.canvas
+ * @param {number} param0.value
+ * @param {number} param0.minValue
+ * @param {number} param0.maxValue
+ * @param {string} param0.color
+ */
+CanvasUtil.drawValueOnCanvasLastColumn = ({
+  canvas,
+  value,
+  minValue,
+  maxValue,
+  color,
+}) => {
+  let context = canvas.getContext("2d");
 
   // draw point representing value on last canvas row
-  context.fillStyle = color
+  context.fillStyle = color;
   context.fillRect(
-    canvas.width-1,CanvasUtil.invertY(
+    canvas.width - 1,
+    CanvasUtil.invertY(
       canvas,
-      Util.linearConversionWithMaxAndMin(
-        {
-          fromBaseMin: minValue,
-          fromBaseMax: maxValue,
-          toBaseMin: 0,
-          toBaseMax: canvas.height,
-          valueToConvert: value
-        }
-      )
-    ),1,1)
-}
+      Util.linearConversionWithMaxAndMin({
+        fromBaseMin: minValue,
+        fromBaseMax: maxValue,
+        toBaseMin: 0,
+        toBaseMax: canvas.height,
+        valueToConvert: value,
+      }),
+    ),
+    1,
+    1,
+  );
+};
 
-// move whole canvas image to left, loosing first column and clearing last
+/**
+ * Move whole canvas image to left, loosing first column and clearing last
+ * @param {HTMLCanvasElement} canvas
+ */
 CanvasUtil.shiftLeft = (canvas) => {
-  let context = canvas.getContext('2d')
+  let context = canvas.getContext("2d");
 
   //copy image onto itself, one pixel to the left
-  let imgData = context.getImageData(1, 0, canvas.width, canvas.height)
-  context.putImageData(imgData, 0, 0)
+  let imgData = context.getImageData(1, 0, canvas.width, canvas.height);
+  context.putImageData(imgData, 0, 0);
 
   // clear last row
-  context.clearRect(canvas.width-1, 0, canvas.width, canvas.height)
+  context.clearRect(canvas.width - 1, 0, canvas.width, canvas.height);
+};
 
-}
-
-export {CanvasUtil as default}
-export {CanvasUtil}
+export { CanvasUtil, CanvasUtil as default };
