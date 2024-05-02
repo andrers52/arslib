608dc7231a3d5fb341ba42f5c4168fcafcb4facd
andrers52 <rs.andre@gmail.com>
2024-05-02 02:14:24 -0300

    removing evoImg

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 // data structures
-export { Fifo } from './data-structures/fifo.js'
+export { Fifo } from "./data-structures/fifo.js";
 // enhancements
-export { EArray } from './enhancements/e-array.js'
-export { EFunction } from './enhancements/e-function.js'
-export { EObject } from './enhancements/e-object.js'
-export { EString } from './enhancements/e-string.js'
+export { EArray } from "./enhancements/e-array.js";
+export { EFunction } from "./enhancements/e-function.js";
+export { EObject } from "./enhancements/e-object.js";
+export { EString } from "./enhancements/e-string.js";
 // mixins
-export { Observable } from './mixins/observable.js'
+export { Observable } from "./mixins/observable.js";
 // node/mixins
-export { NodeLogToFile } from './node/mixins/node-log-to-file.js'
+export { NodeLogToFile } from "./node/mixins/node-log-to-file.js";
 // node
-export { NodeConsoleLog } from './node/node-console-log.js'
-export { NodeHttpRequest } from './node/node-http-request.js'
+export { NodeConsoleLog } from "./node/node-console-log.js";
+export { NodeHttpRequest } from "./node/node-http-request.js";
 // time
-export { TimeConstrainedAction } from './time/time-constrained-action.js'
-export { Time } from './time/time.js'
+export { TimeConstrainedAction } from "./time/time-constrained-action.js";
+export { Time } from "./time/time.js";
 // util
-export { Assert } from './util/assert.js'
-export { CanvasUtil } from './util/canvas-util.js'
-export { Cookie } from './util/cookie.js'
-export { evoImg } from './util/evo-img.js'
-export { ImageUtil } from './util/image-util.js'
-export { LanguageConstruct } from './util/language-construct.js'
-export { OrderedArray } from './util/ordered-array.js'
-export { Platform } from './util/platform.js'
-export { Random } from './util/random.js'
+export { Assert } from "./util/assert.js";
+export { CanvasUtil } from "./util/canvas-util.js";
+export { Cookie } from "./util/cookie.js";
+export { ImageUtil } from "./util/image-util.js";
+export { LanguageConstruct } from "./util/language-construct.js";
+export { OrderedArray } from "./util/ordered-array.js";
+export { Platform } from "./util/platform.js";
+export { Random } from "./util/random.js";
 // util/singleton
-export { CommUtil } from './util/singleton/comm-util.js'
-export { CommWSUtil } from './util/singleton/comm-ws-util.js'
-export { FileStore } from './util/singleton/file-store.js'
-export { Localization } from './util/singleton/localization.js'
-export { Persistence } from './util/singleton/persistence.js'
-export { Util } from './util/util.js'
-
+export { CommUtil } from "./util/singleton/comm-util.js";
+export { CommWSUtil } from "./util/singleton/comm-ws-util.js";
+export { FileStore } from "./util/singleton/file-store.js";
+export { Localization } from "./util/singleton/localization.js";
+export { Persistence } from "./util/singleton/persistence.js";
+export { Util } from "./util/util.js";
