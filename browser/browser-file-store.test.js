b6b276596acecd97dfbe82e84bac95aef024a835
andrers52 <rs.andre@gmail.com>
2025-12-18 19:12:45 -0300

    added file store test

--- browser/browser-file-store.test.js
+++ browser/browser-file-store.test.js
@@ -1,21 +1,27 @@
-import { strict as assert } from "assert";
 import { Platform } from "../platform.js";
+import { TestRunner, expect } from "../test/test-runner.js";
 import { BrowserFileStore } from "./browser-file-store.js";
 
-describe("BrowserFileStore", function() {
-  it("should export an empty object in Node.js environment", function() {
+const runner = new TestRunner();
+
+runner.test(
+  "BrowserFileStore should export an empty object in Node.js environment",
+  () => {
     if (Platform.isNode()) {
-      assert.strictEqual(
+      expect.toBe(
         Object.keys(BrowserFileStore).length,
         0,
-        "BrowserFileStore should be an empty object in Node.js"
+        "BrowserFileStore should be an empty object in Node.js",
       );
     } else {
       // In a browser environment, BrowserFileStore should not be empty
-      assert.ok(
-        Object.keys(BrowserFileStore).length > 0,
-        "BrowserFileStore should not be an empty object in a browser environment"
+      expect.toBeGreaterThan(
+        Object.keys(BrowserFileStore).length,
+        0,
+        "BrowserFileStore should not be an empty object in a browser environment",
       );
     }
-  });
-});
+  },
+);
+
+runner.run();
